4526083e4d3495dd292cf774c996829206916b1d
low zi xiang <lowzixiang@graduate.utm.my>
2023-06-24 20:22:12 +0800

    code smell:duplicated

--- resources/views/pages/report-item-Overview.blade.php
+++ resources/views/pages/report-item-Overview.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
+
     @include('layouts.navbars.auth.topnav', ['title' => 'Projects'])
+    <?php
+$start = microtime(true); 
+?>
     <div class="card shadow-lg mx-4 card-profile-bottom">
         <div class="card-body p-3">
             <h4>Loan Overview</h4>
@@ -10,68 +14,33 @@
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    <div class="card-body p-3">
-                        <div class="row">
-                            <div class="col-8">
-                                <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Assets</p>
-                                    <h5 class="font-weight-bolder">
-                                        {{ $itemCount['asset'] }}
-                                    </h5>
-
-                                </div>
-                            </div>
-                            <div class="col-4 text-end">
-                                <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
-                                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            @component('components.card', [
+                'label' => 'Total Assets',
+                'value' => $itemCount['asset'],
+                'bgColor' => 'bg-gradient-primary shadow-primary',
+                'iconClass' => 'ni ni-money-coins'
+            ])
+            @endcomponent
+            </div>
+            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+            @component('components.card', [
+                'label' => 'Total Inventories',
+                'value' => $itemCount['inventory'],
+                'bgColor' => 'bg-gradient-danger shadow-danger',
+                'iconClass' => 'ni ni-world'
+            ])
+            @endcomponent
             </div>
             <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
                     <div class="card-body p-3">
-                        <div class="row">
-                            <div class="col-8">
-                                <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Inventories</p>
-                                    <h5 class="font-weight-bolder">
-                                        {{ $itemCount['inventory'] }}
-                                    </h5>
-
-                                </div>
-                            </div>
-                            <div class="col-4 text-end">
-                                <div class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
-                                    <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    <div class="card-body p-3">
-                        <div class="row">
-                            <div class="col-8">
-                                <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Value</p>
-                                    <h5 class="font-weight-bolder">
-                                        RM {{ $totalValue }}
-                                    </h5>
-
-                                </div>
-                            </div>
-                            <div class="col-4 text-end">
-                                <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
-                                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                        </div>
+                    @component('components.card', [
+                'label' => 'Total Value',
+                'value' => 'RM '.$totalValue,
+                'bgColor' => 'bg-gradient-success shadow-success',
+                'iconClass' => 'ni ni-paper-diploma'
+            ])
+            @endcomponent
                     </div>
                 </div>
             </div>
@@ -136,11 +105,16 @@
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
+    <?php
+    $end = microtime(true); // End the timer
 
+$executionTime = $end - $start; // Calculate the execution time in seconds
+
+echo "Execution Time: " . $executionTime . " microseconds";
+?>
     @include('layouts.footers.auth.footer')
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -149,5 +123,6 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="{{ asset('assets/js/project.js') }}"></script>
 
+    echo "Execution Time: ";
     @include('pages.chart.item-overview-chart');
 @endsection
